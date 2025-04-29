ceda2446546af83e54ee065069331f08e8fa7a5b
TarunTeja-123 <taruntejanethi@gmail.com>
2025-04-30 00:04:27 +0530

    Expensed code updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    user_expenses = {}
+
+    Expense.pluck(:splitting_users_ids).each do |splitting_user_ids|
+      splitting_user_ids.each do |user_id, expense_amount|
+        if user_expenses[user_id]
+          user_expenses[user_id] += expense_amount
+        else
+          user_expenses[user_id] = expense_amount
+        end
+      end
+    end
+
+    expense_data = user_expenses.map do |user_id, total_expense|
+      user = User.find(user_id)
+      { user_name: user.name, total_expense: total_expense }
+    end
+
+    render json: { expensed_data: expense_data }
+  end
+
   def permitted_params
     params.require(:user).permit(:name, :email, :password_digest)
   end
